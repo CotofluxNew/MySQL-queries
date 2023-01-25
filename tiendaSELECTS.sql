1526c6917018a7bf0ba35eaa0670fb7527c51da5
CotofluxNew <cotoflux@gmail.com>
2023-01-25 13:06:28 +0100

    Guardo cambios de hoy

--- tiendaSELECTS.sql
+++ tiendaSELECTS.sql
@@ -1,41 +1,95 @@
-Llista el nom de tots els productes que hi ha en la taula "producto".
-Llista els noms i els preus de tots els productes de la taula "producto".
-Llista totes les columnes de la taula "producto".
-Llista el nom dels "productos", el preu en euros i el preu en dòlars nord-americans (USD).
-Llista el nom dels "productos", el preu en euros i el preu en dòlars nord-americans. Utilitza els següents àlies per a les columnes: nom de "producto", euros, dòlars nord-americans.
-Llista els noms i els preus de tots els productes de la taula "producto", convertint els noms a majúscula.
-Llista els noms i els preus de tots els productes de la taula "producto", convertint els noms a minúscula.
-Llista el nom de tots els fabricants en una columna, i en una altra columna obtingui en majúscules els dos primers caràcters del nom del fabricant.
-Llista els noms i els preus de tots els productes de la taula "producto", arrodonint el valor del preu.
-Llista els noms i els preus de tots els productes de la taula "producto", truncant el valor del preu per a mostrar-lo sense cap xifra decimal.
-Llista el codi dels fabricants que tenen productes en la taula "producto".
-Llista el codi dels fabricants que tenen productes en la taula "producto", eliminant els codis que apareixen repetits.
-Llista els noms dels fabricants ordenats de manera ascendent.
-Llista els noms dels fabricants ordenats de manera descendent.
-Llista els noms dels productes ordenats, en primer lloc, pel nom de manera ascendent i, en segon lloc, pel preu de manera descendent.
-Retorna una llista amb les 5 primeres files de la taula "fabricante".
-Retorna una llista amb 2 files a partir de la quarta fila de la taula "fabricante". La quarta fila també s'ha d'incloure en la resposta.
-Llista el nom i el preu del producte més barat. (Utilitza solament les clàusules ORDER BY i LIMIT). NOTA: Aquí no podries usar MIN(preu), necessitaries GROUP BY
-Llista el nom i el preu del producte més car. (Fes servir solament les clàusules ORDER BY i LIMIT). NOTA: Aquí no podries usar MAX(preu), necessitaries GROUP BY.
-Llista el nom de tots els productes del fabricant el codi de fabricant del qual és igual a 2.
-Retorna una llista amb el nom del producte, preu i nom de fabricant de tots els productes de la base de dades.
-Retorna una llista amb el nom del producte, preu i nom de fabricant de tots els productes de la base de dades. Ordena el resultat pel nom del fabricant, per ordre alfabètic.
-Retorna una llista amb el codi del producte, nom del producte, codi del fabricant i nom del fabricant, de tots els productes de la base de dades.
-Retorna el nom del producte, el seu preu i el nom del seu fabricant, del producte més barat.
-Retorna el nom del producte, el seu preu i el nom del seu fabricant, del producte més car.
-Retorna una llista de tots els productes del fabricant Lenovo.
-Retorna una llista de tots els productes del fabricant Crucial que tinguin un preu major que 200 €.
-Retorna una llista amb tots els productes dels fabricants Asus, Hewlett-Packard i Seagate. Sense utilitzar l'operador IN.
-Retorna un llistat amb tots els productes dels fabricants Asus, Hewlett-Packard i Seagate. Usant l'operador IN.
-Retorna un llistat amb el nom i el preu de tots els productes dels fabricants el nom dels quals acabi per la vocal e.
-Retorna un llistat amb el nom i el preu de tots els productes dels fabricants dels quals contingui el caràcter w en el seu nom.
-Retorna un llistat amb el nom de producte, preu i nom de fabricant, de tots els productes que tinguin un preu major o igual a 180 €. Ordena el resultat, en primer lloc, pel preu (en ordre descendent) i, en segon lloc, pel nom (en ordre ascendent).
-Retorna un llistat amb el codi i el nom de fabricant, solament d'aquells fabricants que tenen productes associats en la base de dades.
-Retorna un llistat de tots els fabricants que existeixen en la base de dades, juntament amb els productes que té cadascun d'ells. El llistat haurà de mostrar també aquells fabricants que no tenen productes associats.
-Retorna un llistat on només apareguin aquells fabricants que no tenen cap producte associat.
-Retorna tots els productes del fabricant Lenovo. (Sense utilitzar INNER JOIN).
+1.-Llista el nom de tots els productes que hi ha en la taula "producto".
+SELECT nombre 
+from producto;
+2.-Llista els noms i els preus de tots els productes de la taula "producto".
+
+SELECT nombre, precio
+from producto;
+
+3.-Llista totes les columnes de la taula "producto".
+
+SELECT *
+from producto;
+
+4.-Llista el nom dels "productos", el preu en euros i el preu en dòlars nord-americans (USD).
+
+SELECT nombre, precio AS 'Precio en Euros', round(precio*0.92) AS 'Precio en Dolares'
+from producto;
+
+5.-Llista el nom dels "productos", el preu en euros i el preu en dòlars nord-americans. 
+
+SELECT nombre, precio AS 'Precio en Euros', precio*0.92 AS 'Precio en Dolares'
+from producto;
+
+6.-Utilitza els següents àlies per a les columnes: nom de "producto", euros, dòlars nord-americans.
+
+SELECT nombre, precio AS 'Euros', precio*0.92 AS 'Dolares'
+from producto;
+
+7.-Llista els noms i els preus de tots els productes de la taula "producto", convertint els noms a majúscula.
+
+SELECT upper(nombre), precio
+from producto;
+
+8.-Llista els noms i els preus de tots els productes de la taula "producto", convertint els noms a minúscula.
+
+SELECT lower(nombre), precio
+from producto;
+
+9.-Llista el nom de tots els fabricants en una columna, i en una altra columna obtingui en majúscules els dos primers caràcters del nom del fabricant.
+
+SELECT nombre, SUBSTRING(nombre, 1, 2) AS ExtractString
+from fabricante;
+
+10.-Llista els noms i els preus de tots els productes de la taula "producto", arrodonint el valor del preu.
+
+SELECT round(precio)
+from producto
+
+11.-Llista els noms i els preus de tots els productes de la taula "producto", truncant el valor del preu per a mostrar-lo sense cap xifra decimal.
+
+SELECT nombre, round(precio)
+from producto
+
+12.-Llista el codi dels fabricants que tenen productes en la taula "producto".
+
+SELECT producto.nombre, fabricante.nombre
+from producto, fabricante
+WHERE fabricante.codigo = producto.codigo_fabricante
+
+13.-Llista el codi dels fabricants que tenen productes en la taula "producto", eliminant els codis que apareixen repetits.
+
+
+
+14.-Llista els noms dels fabricants ordenats de manera ascendent.
+
+
+
+15.-Llista els noms dels fabricants ordenats de manera descendent.
+16.-Llista els noms dels productes ordenats, en primer lloc, pel nom de manera ascendent i, en segon lloc, pel preu de manera descendent.
+17.-Retorna una llista amb les 5 primeres files de la taula "fabricante".
+18.-Retorna una llista amb 2 files a partir de la quarta fila de la taula "fabricante". La quarta fila també s'ha d'incloure en la resposta.
+19.-Llista el nom i el preu del producte més barat. (Utilitza solament les clàusules ORDER BY i LIMIT). NOTA: Aquí no podries usar MIN(preu), necessitaries GROUP BY
+20.-Llista el nom i el preu del producte més car. (Fes servir solament les clàusules ORDER BY i LIMIT). NOTA: Aquí no podries usar MAX(preu), necessitaries GROUP BY.
+21.-Llista el nom de tots els productes del fabricant el codi de fabricant del qual és igual a 2.
+22.-Retorna una llista amb el nom del producte, preu i nom de fabricant de tots els productes de la base de dades.
+23.- Retorna una llista amb el nom del producte, preu i nom de fabricant de tots els productes de la base de dades. Ordena el resultat pel nom del fabricant, per ordre alfabètic.
+24.-Retorna una llista amb el codi del producte, nom del producte, codi del fabricant i nom del fabricant, de tots els productes de la base de dades.
+25.-Retorna el nom del producte, el seu preu i el nom del seu fabricant, del producte més barat.
+26.-Retorna el nom del producte, el seu preu i el nom del seu fabricant, del producte més car.
+27.-Retorna una llista de tots els productes del fabricant Lenovo.
+28.-Retorna una llista de tots els productes del fabricant Crucial que tinguin un preu major que 200 €.
+29.-Retorna una llista amb tots els productes dels fabricants Asus, Hewlett-Packard i Seagate. Sense utilitzar l operador IN.
+30.-Retorna un llistat amb tots els productes dels fabricants Asus, Hewlett-Packard i Seagate. Usant l operador IN.
+31.-Retorna un llistat amb el nom i el preu de tots els productes dels fabricants el nom dels quals acabi per la vocal e.
+32.-Retorna un llistat amb el nom i el preu de tots els productes dels fabricants dels quals contingui el caràcter w en el seu nom.
+33.-Retorna un llistat amb el nom de producte, preu i nom de fabricant, de tots els productes que tinguin un preu major o igual a 180 €. Ordena el resultat, en primer lloc, pel preu (en ordre descendent) i, en segon lloc, pel nom (en ordre ascendent).
+34.-Retorna un llistat amb el codi i el nom de fabricant, solament d aquells fabricants que tenen productes associats en la base de dades.
+35.-Retorna un llistat de tots els fabricants que existeixen en la base de dades, juntament amb els productes que té cadascun d ells. El llistat haurà de mostrar també aquells fabricants que no tenen productes associats.
+36.-Retorna un llistat on només apareguin aquells fabricants que no tenen cap producte associat.
+37.-Retorna tots els productes del fabricant Lenovo. (Sense utilitzar INNER JOIN).
 Retorna totes les dades dels productes que tenen el mateix preu que el producte més car del fabricant Lenovo. (Sense fer servir INNER JOIN).
-Llista el nom del producte més car del fabricant Lenovo.
-Llista el nom del producte més barat del fabricant Hewlett-Packard.
-Retorna tots els productes de la base de dades que tenen un preu major o igual al producte més car del fabricant Lenovo.
-Llesta tots els productes del fabricant Asus que tenen un preu superior al preu mitjà de tots els seus productes.
+38.-Llista el nom del producte més car del fabricant Lenovo.
+39.-Llista el nom del producte més barat del fabricant Hewlett-Packard.
+40.-Retorna tots els productes de la base de dades que tenen un preu major o igual al producte més car del fabricant Lenovo.
+41.-Llesta tots els productes del fabricant Asus que tenen un preu superior al preu mitjà de tots els seus productes.
--- universitatSELECTS.sql
+++ universitatSELECTS.sql
@@ -63,26 +63,86 @@ Resol les 6 següents consultes utilitzant les clàusules LEFT JOIN i RIGHT JOIN
 
 10.- Retorna un llistat amb els noms de tots els professors/es i els departaments que tenen vinculats/des. El llistat també ha de mostrar aquells professors/es que no tenen cap departament associat. El llistat ha de retornar quatre columnes, nom del departament, primer cognom, segon cognom i nom del professor/a. El resultat estarà ordenat alfabèticament de menor a major pel nom del departament, cognoms i el nom.
 
+SELECT persona.nombre, persona.apellido1, persona.apellido2, departamento.nombre
+  FROM persona
+  LEFT JOIN departamento
+    ON persona.id = departamento.id
+WHERE persona.tipo = 'profesor'
+ORDER BY persona.apellido1 DESC, persona.apellido2 DESC, persona.nombre DESC;
+
 11.- Retorna un llistat amb els professors/es que no estan associats a un departament.
+
+SELECT persona.nombre, persona.apellido1, persona.apellido2, departamento.nombre
+  FROM persona
+  LEFT JOIN departamento
+    ON persona.id = departamento.id
+WHERE persona.tipo = 'profesor' and departamento.nombre IS NULL
+ORDER BY persona.apellido1 DESC, persona.apellido2 DESC, persona.nombre DESC;
 
 12.- Retorna un llistat amb els departaments que no tenen professors/es associats.
 
+SELECT persona.nombre, persona.apellido1, persona.apellido2, departamento.nombre
+  FROM departamento
+ RIGht JOIN PERSONA
+    ON persona.id = departamento.id
+WHERE persona.nombre is null and persona.apellido1 is null and persona.apellido2 is null
+
 13.- Retorna un llistat amb els professors/es que no imparteixen cap assignatura.
+
+SELECT persona.nombre, persona.apellido1, persona.apellido2, asignatura.nombre
+  FROM asignatura
+ RIGht JOIN persona
+    ON persona.id = asignatura.id_profesor
+WHERE persona.tipo = 'profesor' and asignatura.nombre is null;
 
 14.- Retorna un llistat amb les assignatures que no tenen un professor/a assignat.
 
+SELECT persona.nombre, persona.apellido1, persona.apellido2, asignatura.nombre
+  FROM persona
+ RIGht JOIN asignatura
+    ON persona.id = asignatura.id_profesor
+WHERE asignatura.id_profesor is null
+
 15.- Retorna un llistat amb tots els departaments que no han impartit assignatures en cap curs escolar.
+
+SELECT *
+  FROM asignatura
+ RIGht JOIN departamento 
+    ON departamento.id = asignatura.id_grado 
+Where asignatura.nombre is null
 
 Consultes resum:
 
 16.- Retorna el nombre total d alumnes que hi ha.
+
+SELECT count(*) 
+from alumno_se_matricula_asignatura
+
 17.- Calcula quants/es alumnes van néixer en 1999.
+
+SELECT count(*) 
+from persona
+WHERE persona.fecha_nacimiento BETWEEN '1999-01-01' AND '1999-12-31'
+
 18.- Calcula quants/es professors/es hi ha en cada departament. El resultat només ha de mostrar dues columnes, una amb el nom del departament i una altra amb el nombre de professors/es que hi ha en aquest departament. El resultat només ha d incloure els departaments que tenen professors/es associats i haurà d estar ordenat de major a menor pel nombre de professors/es.
+
+
 19.- Retorna un llistat amb tots els departaments i el nombre de professors/es que hi ha en cadascun d ells. Té en compte que poden existir departaments que no tenen professors/es associats/des. Aquests departaments també han d aparèixer en el llistat.
+
+
 20.- Retorna un llistat amb el nom de tots els graus existents en la base de dades i el nombre d assignatures que té cadascun. Té en compte que poden existir graus que no tenen assignatures associades. Aquests graus també han d aparèixer en el llistat. El resultat haurà d estar ordenat de major a menor pel nombre d assignatures.
+
 21.- Retorna un llistat amb el nom de tots els graus existents en la base de dades i el nombre d assignatures que té cadascun, dels graus que tinguin més de 40 assignatures associades.
+
 22.- Retorna un llistat que mostri el nom dels graus i la suma del nombre total de crèdits que hi ha per a cada tipus d assignatura. El resultat ha de tenir tres columnes: nom del grau, tipus d assignatura i la suma dels crèdits de totes les assignatures que hi ha d aquest tipus.
+
 23.- Retorna un llistat que mostri quants/es alumnes s han matriculat d alguna assignatura en cadascun dels cursos escolars. El resultat haurà de mostrar dues columnes, una columna amb l any d inici del curs escolar i una altra amb el nombre d alumnes matriculats/des.
+
 24.- Retorna un llistat amb el nombre d assignatures que imparteix cada professor/a. El llistat ha de tenir en compte aquells professors/es que no imparteixen cap assignatura. El resultat mostrarà cinc columnes: id, nom, primer cognom, segon cognom i nombre d assignatures. El resultat estarà ordenat de major a menor pel nombre d assignatures.
+
 25.- Retorna totes les dades de l alumne més jove.
+
+SELECT * from persona WHERE persona.fecha_nacimiento  = (SELECT max(persona.fecha_nacimiento)
+from persona);
+
 26.- Retorna un llistat amb els professors/es que tenen un departament associat i que no imparteixen cap assignatura.
